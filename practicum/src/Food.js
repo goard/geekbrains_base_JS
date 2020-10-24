54a830e6d7fab2e0eb1f760ae1c81e93d19d4383
Gonchikov Ayur <hori.tumat@gmail.com>
2020-10-24 18:51:16 +0800

    hw lesson7

---
+++ practicum/src/Food.js
@@ -0,0 +1,47 @@
+class Food {
+  constructor() {
+    this.x = null;
+    this.y = null;
+  }
+
+  /**
+   * @param {Settings} settings
+   * @param {Snake} snake
+   * @param {Board} board
+   */
+  init(settings, snake, board) {
+    this.settings = settings;
+    this.snake = snake;
+    this.board = board;
+  }
+
+  /**
+   *
+   */
+  setNewFood() {
+    const food = this.generateRandomCoordinates();
+    this.board.renderFood(food);
+  }
+
+  /**
+   *
+   */
+  setFood() {
+    this.board.renderFood(this);
+  }
+  /**
+   * @returns {Food}
+   */
+  generateRandomCoordinates() {
+    while (true) {
+      this.x = Math.floor(Math.random() * this.settings.colsCount) + 1;
+      this.y = Math.floor(Math.random() * this.settings.rowsCount) + 1;
+      let cell = this.board.getCellEl(this.x, this.y);
+
+      if (cell.classList.contains('snakeBody')) {
+        continue
+      }
+      return this;
+    }
+  }
+}
